c23e94e02d64445f3486364d3ab44bbfd72c0877
İlker Çetin <97ilkercetin@gmail.com>
2022-05-01 23:52:17 +0300

    Initial commit

--- src/Components/Color.js
+++ src/Components/Color.js
@@ -2,9 +2,18 @@ import React, {useState} from 'react';
 
 function Color(props) {
     const [codeVisibility, setCodeVisibility] = useState(false);
+    const [colorText, setColorText] = useState(props.hexColor.toUpperCase());
 
     function handleHover(shouldShow) {
         setCodeVisibility(shouldShow);
+    }
+
+    function changeColorText () {
+        setColorText('COPIED!');
+
+        setTimeout(() => {
+            setColorText(props.hexColor.toUpperCase());
+        }, 3000);
     }
 
     return (
@@ -14,11 +23,13 @@ function Color(props) {
              onMouseEnter={() => handleHover(true)}
              onMouseLeave={() => handleHover(false)}
              onClick={() => {
-                 navigator.clipboard.writeText(props.hexColor.toUpperCase())
+                 navigator.clipboard.writeText(props.hexColor.toUpperCase());
+
+                 changeColorText();
              }}>
             <div className={'color-text'} style={{
                 display: codeVisibility ? 'block' : 'none',
-            }}>{props.hexColor.toUpperCase()}</div>
+            }}>{colorText}</div>
         </div>
     );
 }
